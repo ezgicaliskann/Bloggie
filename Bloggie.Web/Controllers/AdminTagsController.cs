cf17e1e28e735abc02e4d81785768b4e54c2f6af
ezgicaliskann <casezgi@gmail.com>
2023-03-11 15:00:19 +0300

    Add View oluşturuldu ve view'dan gelen veriler controller'a gönderildi.

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -4,9 +4,18 @@ namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag()
+        {
+            var name = Request.Form["name"];
+            var displayName = Request.Form["displayName"];
+            return View("Add");
         }
     }
 }
