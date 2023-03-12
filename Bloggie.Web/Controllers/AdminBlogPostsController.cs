29630530c84c3f48a7bde1bca26f04a73e4862eb
ezgicaliskann <casezgi@gmail.com>
2023-03-12 16:33:56 +0300

    Admin Blog Post Controller, view , viewmodel oluşturuldu. Add metodu başladı.

---
+++ Bloggie.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,30 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        private readonly ITagInterface tagRepository;
+        public AdminBlogPostsController(ITagInterface tagRespository)
+        {
+            this.tagRepository = tagRespository;   
+        }
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var tags = await tagRepository.GetAllAsync();
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
+            return View(model);
+        }
+    }
+}
