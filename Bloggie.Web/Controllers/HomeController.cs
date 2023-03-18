ae485964e4444a65f6db1b8bdd6a8c755dbe1270
ezgicaliskann <casezgi@gmail.com>
2023-03-18 12:25:22 +0300

    Blog Home Page Tasarlandı. Blog Detay Sayfası Tasarlandı. Repository'e eklendi.

--- Bloggie.Web/Controllers/HomeController.cs
+++ Bloggie.Web/Controllers/HomeController.cs
@@ -1,32 +1,37 @@
 ﻿using Bloggie.Web.Models;
+using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace Bloggie.Web.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IBlogPostRepository blogPostRepository;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
+		{
+			_logger = logger;
+			this.blogPostRepository = blogPostRepository;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public async Task<IActionResult> Index()
+		{
+			var blogPosts = await blogPostRepository.GetAllAsync();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+			return View(blogPosts);
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
